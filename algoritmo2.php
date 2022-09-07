6afaa7eb89005fee72d35f0655cb4fe57a5f96a6
Crislander94 <crhighlander94@gmail.com>
2022-09-06 19:35:36 -0500

    Primer commit, algoritmo 1 y 2 terminado, falta uno

---
+++ algoritmo2.php
@@ -0,0 +1,27 @@
+<?php
+    // Retorna true si el paramentro de entrada
+    // es cuadrado perfecto
+    function isPerfectSquare($num){
+        $resp = (int)(sqrt($num));
+        return ($resp * $resp == $num);
+    }
+    // verifica si es fibonacci
+    function isFibonacci($n){
+        // n es Fibonacci si es cumple
+        // 5*n*n + 4 o 5*n*n - 4 
+        // o son cuadrados perfectos
+        return isPerfectSquare(5 * $n * $n + 4) ||
+        isPerfectSquare(5 * $n * $n - 4);
+    }
+
+
+    $resultado = isFibonacci($_POST["dato1"]);
+
+    echo $resultado === true ? "Es un numero fibonacci" : "No es un numero fibonacci";
+
+?>
+
+
+<pre>
+    <a href="index.php">Inicio</a>
+</pre>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis Pruebas</title>
+</head>
+<body>
+    <h3>Algoritmo Numeros primos</h3>
+    <form action="n_numeros.php" method="post">
+        <input type="text" name="dato1" onkeypress="ValidaSoloNumeros()" placeholder="ingresa un numero">
+        <input type="submit" value="Probar">
+    </form>
+
+    <h3>Algoritmo Fibonacci</h3>
+    <form action="algoritmo2.php" method="post">
+        <input type="text" name="dato1" onkeypress="ValidaSoloNumeros()" placeholder="ingresa un numero">
+        <input type="submit" value="Probar">
+    </form>
+
+    <h3>Algoritmo Arbol Genealogico</h3>
+    <form action="algoritmo3.php" method="post">
+        <input type="text" name="dato1" onkeypress="ValidaSoloNumeros()" placeholder="ingresa un numero">
+        <input type="submit" value="Probar">
+    </form>
+</body>
+</html>
+
+<script>
+    // Validación de solo numeros
+    function ValidaSoloNumeros() {
+        //Verifica por codigo ASCII que el caracter ingresado sea numerico
+        if ((event.keyCode < 48) || (event.keyCode > 57))
+            event.returnValue = false;
+    }
+</script>
